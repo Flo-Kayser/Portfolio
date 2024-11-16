e3b4443c39acaa0fbbecbd72d0dde565060b7e6f
Flo-Kayser <flokayser.fk@gmail.com>
2024-11-16 20:07:28 +0100

    add: Gallery, Footer, Home finished Projects
    add: Gallery, Footer, Home
    finished Projects

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 // main.js
 
+// ========== Home ==========
+// ========== Typewriter ==========
+var typed = new Typed(".auto-type", {
+  strings: ["Flo Kayser!", "23 years old!", "still studying!", "a Programmer!", "an 3D - Artist!"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true
+})
+
+
+
+
+
+
 // navbar
 
 let toggleNavbar = document.querySelector(".toggle-navbar");
@@ -9,45 +23,75 @@ toggleNavbar.onclick = function () {
   nav.classList.toggle("active");
 };
 
-// Email Js
-const contactForm = document.getElementById("contact-form"),
-  contactMessage = document.getElementById("contact-message");
 
-const sendEmail = (e) => {
-  e.preventDefault();
+// ========== Gallery ==========
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
 
-  emailjs
-    .sendForm(
-      "service_aru0tga",
-      "template_a1hyl64",
-      contactForm,
-      "mMO3ECnvSBqtJeYH6"
-    )
-    .then(() => {
-      contactMessage.textContent = "Message sent successfully! ";
-    });
+next.addEventListener('click', function () {
+  let items = document.querySelectorAll('.gallery-item')
+  let frontItem = items[2];
+  document.querySelector('.gallery-slide').appendChild(items[0])
 
-  setTimeout(() => {
-    contactMessage.textContent = "";
-  }, 5000);
+  let video = frontItem.querySelector('video');
+  if (video) {
+    video.pause();
+    video.currentTime = 0;
+    video.play();
+  }
+})
 
-  contactForm.reset();
-};
-(error) => {
-  contactMessage.textContent = "Error sending message. Please try again.";
-  console.error("EmailJS Error:", error);
-};
-contactForm.addEventListener("submit", sendEmail);
+prev.addEventListener('click', function () {
+  let items = document.querySelectorAll('.gallery-item')
+  let frontItem = items[0];
+  document.querySelector('.gallery-slide').prepend(items[items.length - 1])
+
+  let video = frontItem.querySelector('video');
+  if (video) {
+    video.pause();
+    video.currentTime = 0;
+    video.play();
+  }
+})
 
 
 // ========== Projects ==========
 let projectBox = document.querySelectorAll('.project-box');
-projectBox.forEach(projectBox =>{
-  projectBox.onmousemove = function(e){
-    let x =e.pageX -projectBox.offsetLeft;
-    let y =e.pageY -projectBox.offsetTop;
+projectBox.forEach(projectBox => {
+  projectBox.onmousemove = function (e) {
+    let x = e.pageX - projectBox.offsetLeft;
+    let y = e.pageY - projectBox.offsetTop;
 
-    projectBox.style.setProperty('--x', x+'px');
-    projectBox.style.setProperty('--y', y+'px');
+    projectBox.style.setProperty('--x', x + 'px');
+    projectBox.style.setProperty('--y', y + 'px');
   }
+  // Email Js
+  const contactForm = document.getElementById("contact-form"),
+    contactMessage = document.getElementById("contact-message");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_aru0tga",
+        "template_a1hyl64",
+        contactForm,
+        "mMO3ECnvSBqtJeYH6"
+      )
+      .then(() => {
+        contactMessage.textContent = "Message sent successfully! ";
+      });
+
+    setTimeout(() => {
+      contactMessage.textContent = "";
+    }, 5000);
+
+    contactForm.reset();
+  };
+  (error) => {
+    contactMessage.textContent = "Error sending message. Please try again.";
+    console.error("EmailJS Error:", error);
+  };
+  contactForm.addEventListener("submit", sendEmail);
 })
